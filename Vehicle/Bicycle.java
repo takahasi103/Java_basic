665a126fa4eecc33b1f1e607eb626cc434def316
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-07-03 22:09:49 +0900

    [Add] person

--- Vehicle/Bicycle.java
+++ Vehicle/Bicycle.java
@@ -1,5 +1,11 @@
-package Vehicle;
-
-public class Bicycle {
-    
-}
+class Bicycle extends Vehicle {
+    Bicycle(String name, String color) {
+      super(name, color);
+    }
+  
+    public void run(int distance) {
+      System.out.println(distance + "km走ります");
+      this.distance += distance;
+      System.out.println("走行距離：" + this.distance + "km");
+    }
+  }
