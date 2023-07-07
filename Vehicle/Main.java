9413171e1f1ceb0fc7310034a9287fcbc3a1a046
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-07-07 22:40:44 +0900

    [Add] vehicle selection

--- Vehicle/Main.java
+++ Vehicle/Main.java
@@ -15,39 +15,44 @@ class Main {
       System.out.print("体重(kg):");
       double weight = scanner.nextDouble();
       Person person1 = new Person(firstName, lastName, age, height, weight);
-      Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
-
+      
+      
       Car car = new Car("フェラーリ", "赤");
       Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+      System.out.println("乗り物を選んで下さい");
+      System.out.print("1:自動車, 2:自転車 数字を教えて:");
+      int myVehicle = scanner.nextInt();
       
-      person1.buy(car);
-      
-      person2.buy(bicycle);
+      if (1 == myVehicle) {
+        person1.buy(car);
 
-      System.out.println("【車の情報】");
-      car.printData();
-      System.out.println("-----------------");
-      System.out.println("【車の所有者の情報】");
-      car.getOwner().printData();
-      System.out.println("-----------------");
-      System.out.print("走る距離を入力してください：");
-      int carDistance = scanner.nextInt();
-      car.run(carDistance);
-      System.out.println("-----------------");
-      System.out.print("給油する量を入力してください：");
-      int litre = scanner.nextInt();
-      car.charge(litre);
+        System.out.println("【車の情報】");
+        car.printData();
+        System.out.println("-----------------");
+        System.out.println("【車の所有者の情報】");
+        car.getOwner().printData();
+        System.out.println("-----------------");
+        System.out.print("走る距離を入力してください：");
+        int carDistance = scanner.nextInt();
+        car.run(carDistance);
+        System.out.println("-----------------");
+        System.out.print("給油する量を入力してください：");
+        int litre = scanner.nextInt();
+        car.charge(litre);
+      } else {
+        person1.buy(bicycle);
 
-      System.out.println("=================");
-      System.out.println("【自転車の情報】");
-      bicycle.printData();
-      System.out.println("-----------------");
-      System.out.println("【自転車の所有者の情報】");
-      bicycle.getOwner().printData();
-      System.out.println("-----------------");
-      System.out.print("走る距離を入力してください：");
-      int bicycleDistance = scanner.nextInt();
-      bicycle.run(bicycleDistance);
+        System.out.println("=================");
+        System.out.println("【自転車の情報】");
+        bicycle.printData();
+        System.out.println("-----------------");
+        System.out.println("【自転車の所有者の情報】");
+        bicycle.getOwner().printData();
+        System.out.println("-----------------");
+        System.out.print("走る距離を入力してください：");
+        int bicycleDistance = scanner.nextInt();
+        bicycle.run(bicycleDistance);
+      }
     }
   }
 }
