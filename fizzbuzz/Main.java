a79f4382d8717dbf992ebc91e01885b04ad40a2d
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-07-28 13:49:20 +0900

    [Fix] FizzBuzz

--- fizzbuzz/Main.java
+++ fizzbuzz/Main.java
@@ -3,13 +3,15 @@ package fizzbuzz;
 public class Main {
     public static void main(String[] args) {
         int n = 100; // 表示する整数の上限値
+        int numBuzz = 3;
+        int numFizz = 5;
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % numBuzz == 0 && i % numFizz == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (i % numBuzz == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (i % numFizz == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
