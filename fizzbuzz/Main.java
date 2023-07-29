8c870fd12ad3d7638d0ac519abb3b92885d76f84
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-07-29 12:21:33 +0900

    [Add] arrangement

--- fizzbuzz/Main.java
+++ fizzbuzz/Main.java
@@ -1,4 +1,6 @@
 package fizzbuzz;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -6,16 +8,35 @@ public class Main {
         int numBuzz = 3;
         int numFizz = 5;
 
+        // for (int i = 1; i <= n; i++) {
+        //     if (i % numBuzz == 0 && i % numFizz == 0) {
+        //         System.out.println("FizzBuzz");
+        //     } else if (i % numBuzz == 0) {
+        //         System.out.println("Fizz");
+        //     } else if (i % numFizz == 0) {
+        //         System.out.println("Buzz");
+        //     } else {
+        //         System.out.println(i);
+        //     }
+        // }
+
+        ArrayList<Integer> FizzBuzz = new ArrayList<Integer>();
+        ArrayList<Integer> Fizz = new ArrayList<Integer>();
+        ArrayList<Integer> Buzz = new ArrayList<Integer>();
+
         for (int i = 1; i <= n; i++) {
             if (i % numBuzz == 0 && i % numFizz == 0) {
-                System.out.println("FizzBuzz");
+                FizzBuzz.add(i);
             } else if (i % numBuzz == 0) {
-                System.out.println("Fizz");
+                Buzz.add(i);
             } else if (i % numFizz == 0) {
-                System.out.println("Buzz");
+                Fizz.add(i);
             } else {
-                System.out.println(i);
+                continue;
             }
         }
+        System.out.println(FizzBuzz);
+        System.out.println(Buzz);
+        System.out.println(Fizz);
     }
 }
