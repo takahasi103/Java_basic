83a2379f289382aeb29dfc2abb91636b9996fd10
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-07-30 20:19:05 +0900

    [Add] Proof

--- fizzbuzz/Main.java
+++ fizzbuzz/Main.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 
 
 public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         int n = 100; // 表示する整数の上限値
         int numBuzz = 3;
@@ -44,6 +47,23 @@ public class Main {
         System.out.println("");
 
         System.out.println("Fizz");
+        System.out.println(Fizz);
+        System.out.println("");
+
+        // ArrayListの場合、要素の数を取得するにはsize()メソッドを使用
+        for (int i = 0; i < Buzz.size(); i++) {
+            Buzz.set(i, Buzz.get(i) % 3);
+        }
+
+        for (int i = 0; i < Fizz.size(); i++) {
+            Fizz.set(i, Fizz.get(i) % 5);
+        }
+
+        System.out.println("Buzzの要素は3で割るとあまりが0");
+        System.out.println(Buzz);
+        System.out.println("");
+
+        System.out.println("Fizzの要素は5で割るとあまりが0");
         System.out.println(Fizz);
         System.out.println("");
     }
